7c09067f93303361c00b2dfc9a19a4a868cde757
Madeleine Lerner <116823601+mlerner27@users.noreply.github.com>
2023-12-11 15:45:13 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,395 @@
+export const listings = [
+
+    {
+
+      id: 1,
+
+      image1: require('../../assets/images/swipe1.png'),
+
+      //NO IMAGES EXIST 
+
+      image2: require('../../assets/images/swipe1_2.png'),
+
+      image3: require('../../assets/images/swipe1_3.png'),
+
+      image4: require('../../assets/images/swipe1_4.png'),
+
+      //
+
+      address: '6th Ave & 9th St',
+
+      neighborhood: 'GREENWICH VILLAGE',
+
+      bed_bath: '3 bed : 2 bath',
+
+      price: '$2000/month',
+
+      date_range: 'May 31 - August 15',
+
+      host: 'Julia',
+
+      subway: require('../../assets/images/subway1.png'),
+
+      amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+      map: require('../../assets/images/map1.png'),
+
+    },
+
+    {
+
+        id: 2,
+
+        image1: require('../../assets/images/swipe2.png'),
+
+        address: 'Amsterdam & 74th St',
+
+        neighborhood: 'UPPER WEST SIDE',
+
+        bed_bath: '1 bed : 1 bath',
+
+        price: '$2850/month',
+
+        date_range: 'June 1 - August 1',
+
+        host: 'Jack',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+    },
+
+      {
+
+        id: 3,
+
+        image1: require('../../assets/images/swipe3.png'),
+
+        address: 'Malcom X & 122nd St',
+
+        neighborhood: 'HARLEM',
+
+        bed_bath: 'Studio',
+
+        price: '$2200/month',
+
+        date_range: 'May 15 - August 30',
+
+        host: 'Adina',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 4,
+
+        image1: require('../../assets/images/swipe4.png'),
+
+        address: 'Mercer & Prince St',
+
+        neighborhood: 'SOHO',
+
+        bed_bath: '3 bed : 2 bath',
+
+        price: '$2150/month',
+
+        date_range: 'May 1 - September 1',
+
+        host: 'Matt',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 5,
+
+        image1: require('../../assets/images/swipe5.png'),
+
+        address: 'York & 78th St',
+
+        neighborhood: 'LENNOX HILL',
+
+        bed_bath: '3 bed : 1 bath',
+
+        price: '$2100/month',
+
+        date_range: 'April 1 - July 1',
+
+        host: 'Max',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 6,
+
+        image1: require('../../assets/images/swipe6.png'),
+
+        address: 'Exchange & William',
+
+        neighborhood: 'FINANCIAL DISTRICT',
+
+        bed_bath: 'Studio',
+
+        price: '$3000/month',
+
+        date_range: 'June 1 - August 30',
+
+        host: 'Madeleine',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 7,
+
+        image1: require('../../assets/images/swipe7.png'),
+
+        address: 'Broadway & 91st',
+
+        neighborhood: 'UPPER WEST SIDE',
+
+        bed_bath: 'Studio',
+
+        price: '$2700/month',
+
+        date_range: 'May 30 - August 30',
+
+        host: 'Luciana',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 8,
+
+        image1: require('../../assets/images/swipe8.png'),
+
+        address: '5th & 139th',
+
+        neighborhood: 'CENTRAL HARLEM',
+
+        bed_bath: '1 bed : 1 bath',
+
+        price: '$2100/month',
+
+        date_range: 'June 1 - September 30',
+
+        host: 'Jane',
+
+        subway: require('../../assets/images/subway1.png'),
+
+        amenities: ['Wifi', 'Gym', 'Laundry', 'Parking'],
+
+        map: require('../../assets/images/map1.png'),
+
+      },
+
+      {
+
+        id: 9,
+
+        image1: require('../../assets/images/swipe9.png'),
+
+        address: '8th Ave & W 23rd St',
+
+        neighborhood: 'CHELSEA',
+
+        bed_bath: '2 bed : 2 bath',
+
+        price: '$3500/month',
+
+        date_range: 'June 30 - July 15',
+
+        host: 'Madeleine',
+
+        subway: require('../../assets/images/subway123.png'),
+
+        amenities: ['Wifi'],
+
+        map: require('../../assets/images/map9.png'),
+
+      }
+
+];
+
+
+
+export const previews = [
+
+    {
+
+      id: 1,
+
+      image: require('../../assets/images/circle1.png')
+
+    },
+
+    {
+
+      id: 2,
+
+      image: require('../../assets/images/circle2.png')
+
+    },
+
+    {
+
+      id: 3,
+
+      image: require('../../assets/images/circle3.png')
+
+    },
+
+    {
+
+      id: 4,
+
+      image: require('../../assets/images/circle4.png')
+
+    },
+
+    {
+
+      id: 5,
+
+      image: require('../../assets/images/circle5.png')
+
+    },
+
+    {
+
+      id: 6,
+
+      image: require('../../assets/images/circle6.png')
+
+    },
+
+    {
+
+      id: 7,
+
+      image: require('../../assets/images/circle7.png')
+
+    }
+
+]
+
+
+
+export const chats = [
+
+    {
+
+      id: 1,
+
+      name_address: "Adam: 6th & 9th",
+
+      message: "Sounds good!",
+
+      image: require('../../assets/images/chat1.png')
+
+    },
+
+    {
+
+      id: 2,
+
+      name_address: "Emily: Lexington & 72nd",
+
+      message: "Are the neighbors loud?",
+
+      image: require('../../assets/images/chat2.png')
+
+    },
+
+    {
+
+      id: 3,
+
+      name_address: "Steven: Franklin & Broadway",
+
+      message: "You can pass by anytime b...",
+
+      image: require('../../assets/images/chat3.png')
+
+    },
+
+    {
+
+      id: 4,
+
+      name_address: "Logan: Amsterdam & 116th",
+
+      message: "My dog's name is Liam!",
+
+      image: require('../../assets/images/chat4.png')
+
+    },
+
+    {
+
+      id: 5,
+
+      name_address: "Adina: Park & 52nd",
+
+      message: "The place is fully furnished",
+
+      image: require('../../assets/images/chat5.png')
+
+    },
+
+    {
+
+      id: 6,
+
+      name_address: "Josh: Broadway & 122th",
+
+      message: "Sorry. No pets.",
+
+      image: require('../../assets/images/chat6.png')
+
+    }
+
+]
+
+
+
+export const saved = [
+
+ 
+
+];
